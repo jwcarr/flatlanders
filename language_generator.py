3521ed7e3da18805a06a52be16b59a02a16ebc27
Jon Carr <j.w.carr@icloud.com>
2013-06-15 19:20:10 +0100

    Added function to generate syllabary from list of consonants and vowels

--- language_generator.py
+++ language_generator.py
@@ -1,19 +1,36 @@
 import random
 
-def generate(syllabary, number_of_words=50, min_syllables=4, max_syllables=4):
+c = ["s", "t", "d", "g", "z", "p", "f", "m"]
+v = ["a", "e", "i", "o", "u"]
+
+def create_language(consonants, vowels, min_syllables=2, max_syllables=4, number_of_words=50):
+    syllabary = create_syllabary(consonants, vowels)
+    language = generate(syllabary, number_of_words, min_syllables, max_syllables)
+    return language
+
+def create_syllabary(consonants, vowels):
+    syllables = []
+    for i in consonants:
+        for j in vowels:
+            syllable = i + j
+            syllables.append(syllable)
+    return syllables
+
+def generate(syllabary, number_of_words, min_syllables, max_syllables):
     words = []
     for i in range(0, number_of_words):
         word = generateWord(syllabary, min_syllables, max_syllables)
         while word in words:
-            word = generateWord(syllabary, min_syllables, max_syllables)
+            word = generate_word(syllabary, min_syllables, max_syllables)
         words.append(word)
         print word
     return
 
-def generateWord(syllabary, min_syllables, max_syllables):
+def generate_word(syllabary, min_syllables, max_syllables):
     number_of_syllables = random.randrange(min_syllables, max_syllables + 1)
     word = ""
     for i in range(0, number_of_syllables):
         syllable = syllabary[random.randrange(0, len(syllabary))]
         word += syllable
     return word
+
