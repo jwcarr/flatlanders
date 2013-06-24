059a463759000de08501dad540523f2f49a52adf
Jon Carr <j.w.carr@icloud.com>
2013-06-24 18:53:11 +0100

    Fixed areaOfTriangle bug
    areaOfTriangle now works correctly

--- geometry.py
+++ geometry.py
@@ -2,6 +2,7 @@ import scipy
 
 #############################################################################
 #   CALCULATE AVERAGE LINE LENGTH
+
 def averageLineLength(x1, y1, x2, y2, x3, y3):
     AB = distanceBetweenPoints(x1, y1, x2, y2)
     BC = distanceBetweenPoints(x2, y2, x3, y3)
@@ -11,15 +12,29 @@ def averageLineLength(x1, y1, x2, y2, x3, y3):
 #############################################################################
 #   CALCULATE THE AREA OF A TRIANGLE
 
-def areaOfTriangle(x1, y1, x2, y2, x3, y3):
+def area(x1, y1, x2, y2, x3, y3):
     base = distanceBetweenPoints(x1, y1, x2, y2)
-    height = distanceBetweenPoints(x2, y2, x3, y3)
-    square_area = base * height
-    triangle_area = square_area / 2.0
-    return triangle_area
+    height = distanceBetweenPoints(x2, y2, x2, y3)
+    return (base * height) / 2.0
 
 #############################################################################
-#   CALCULATE THE DISTANCE BETWEEN TWO COORDINATES
+#   FIND THE CENTROID OF A TRIANGLE
+
+def centroid(x1, y1, x2, y2, x3, y3):
+    return (x1+x2+x3)/3.0, (y1+y2+y3)/3.0
+
+#############################################################################
+#   CALCULATE THE ANGLES OF A TRIANGLE
+
+def angles(x1, y1, x2, y2, x3, y3):
+    AB = distanceBetweenPoints(x1, y1, x2, y2)
+    BC = distanceBetweenPoints(x2, y2, x3, y3)
+    AC = distanceBetweenPoints(x1, y1, x3, y3)
+    h = max(AB, BC, AC)
+    return 
+
+#############################################################################
+#   CALCULATE THE EUCLIDEAN DISTANCE BETWEEN TWO POINTS
 
 def distanceBetweenPoints(x1, y1, x2, y2):
     return scipy.sqrt(((x1 - x2)**2)+((y1 - y2)**2))
