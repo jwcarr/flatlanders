6aff5500a356a34e293d0e917f2e7cba97301b4f
Jon Carr <j.w.carr@icloud.com>
2013-07-10 22:55:42 +0100

    Added triangleDistance()
    Calculates the Euclidean distance between two triangles

--- geometry.py
+++ geometry.py
@@ -1,40 +1,39 @@
 import scipy
 
 #############################################################################
-#   CALCULATE AVERAGE LINE LENGTH
+#   CALCULATE THE PERIMETER OF A TRIANGLE
 
-def averageLineLength(x1, y1, x2, y2, x3, y3):
-    AB = distanceBetweenPoints(x1, y1, x2, y2)
-    BC = distanceBetweenPoints(x2, y2, x3, y3)
-    AC = distanceBetweenPoints(x1, y1, x3, y3)
-    return (AB + BC + AC) / 3.0
+def perimeter(a, b, c):
+    return ED(a,b)+ED(b,c)+ED(a,c)
 
 #############################################################################
 #   CALCULATE THE AREA OF A TRIANGLE
 
-def area(x1, y1, x2, y2, x3, y3):
-    base = distanceBetweenPoints(x1, y1, x2, y2)
-    height = distanceBetweenPoints(x2, y2, x2, y3)
-    return (base * height) / 2.0
+def area(a, b, c):
+    return (ED(a,b)*ED(b,(b[0],c[1])))/2.0
 
 #############################################################################
 #   FIND THE CENTROID OF A TRIANGLE
 
-def centroid(x1, y1, x2, y2, x3, y3):
-    return (x1+x2+x3)/3.0, (y1+y2+y3)/3.0
-
-#############################################################################
-#   CALCULATE THE ANGLES OF A TRIANGLE
-
-def angles(x1, y1, x2, y2, x3, y3):
-    AB = distanceBetweenPoints(x1, y1, x2, y2)
-    BC = distanceBetweenPoints(x2, y2, x3, y3)
-    AC = distanceBetweenPoints(x1, y1, x3, y3)
-    h = max(AB, BC, AC)
-    return 
+def centroid(a, b, c):
+    return (a[0]+b[0]+c[0])/3.0, (a[1]+b[1]+c[1])/3.0
 
 #############################################################################
 #   CALCULATE THE EUCLIDEAN DISTANCE BETWEEN TWO POINTS
 
-def distanceBetweenPoints(x1, y1, x2, y2):
-    return scipy.sqrt(((x1 - x2)**2)+((y1 - y2)**2))
+def ED(point1, point2):
+    return scipy.sqrt(((point1[0]-point2[0])**2)+((point1[1]-point2[1])**2))
+
+#############################################################################
+#   CALCULATE THE EUCLIDEAN DISTANCE BETWEEN TWO TRIANGLES
+
+def triangleDistance(A, B):
+    return ED(A[0],B[0])+min(ED(A[1],B[1])+ED(A[2],B[2]),ED(A[1],B[2])+ED(A[2],B[1]))
+
+#############################################################################
+#   CALCULATE THE DIFFERENCE IN AREA BETWEEN TWO TRIANGLES
+
+def areaDistance(A, B):
+    area_A = area(A[0], A[1], A[2])
+    area_B = area(B[0], B[1], B[2])
+    return scipy.sqrt((area_A-area_B)**2)
