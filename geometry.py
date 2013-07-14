686ee26dea232fc4f56e51649b2b4e2f32c53be9
Jon Carr <j.w.carr@icloud.com>
2013-07-14 11:51:45 +0100

    Added translate() function
    The translate() function translates triangle B so that its orienting
    spot or its centroid aligns with the orienting spot or centroid of
    triangle A.

--- geometry.py
+++ geometry.py
@@ -37,3 +37,24 @@ def areaDistance(A, B):
     area_A = area(A[0], A[1], A[2])
     area_B = area(B[0], B[1], B[2])
     return scipy.sqrt((area_A-area_B)**2)
+
+#############################################################################
+# TRANSLATE THE COORDINATES FOR TRIANGLE B SO THAT ITS CENTROID OR ORIENTING
+# SPOT ALIGNS WITH THAT OF TRIANGLE A
+
+def translate(A, B, alignment="centroid"):
+    if alignment == "centroid":
+        A_centroid = centroid(A[0], A[1], A[2])
+        B_centroid = centroid(B[0], B[1], B[2])
+        x_shift = A_centroid[0] - B_centroid[0]
+        y_shift = A_centroid[1] - B_centroid[1]
+    elif alignment == "spot":
+        x_shift = A[0][0] - B[0][0]
+        y_shift = A[0][1] - B[0][1]
+    B1x = B[0][0] + x_shift
+    B1y = B[0][1] + y_shift
+    B2x = B[1][0] + x_shift
+    B2y = B[1][1] + y_shift
+    B3x = B[2][0] + x_shift
+    B3y = B[2][1] + y_shift
+    return [(B1x, B1y), (B2x, B2y), (B3x, B3y)]
