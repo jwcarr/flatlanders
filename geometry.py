2606bad4bead45799e6ed2b8e787e72823b40c45
Jon Carr <j.w.carr@icloud.com>
2013-07-25 09:07:15 +0100

    Streamlined code
    Streamlined a lot of the code in an attempt to make it more efficient.
    Also tried to make the notation more consistent.

--- geometry.py
+++ geometry.py
@@ -1,22 +1,22 @@
-from scipy import arccos, array, cos, dot, pi, sin, sqrt
+from numpy import arccos, array, cos, dot, pi, sin, sqrt
 
 #############################################################################
-#   CALCULATE THE PERIMETER OF A TRIANGLE
+#   CALCULATE THE PERIMETER OF TRIANGLE A
 
-def perimeter(a, b, c):
-    return ED(a,b)+ED(b,c)+ED(a,c)
-
-#############################################################################
-#   CALCULATE THE AREA OF A TRIANGLE
-
-def area(a, b, c):
-    return (ED(a,b)*ED(b,(b[0],c[1])))/2.0
+def perimeter(A):
+    return ED(A[0],A[1])+ED(A[1],A[2])+ED(A[2],A[0])
 
 #############################################################################
-#   FIND THE CENTROID OF A TRIANGLE
+#   CALCULATE THE AREA OF TRIANGLE A
 
-def centroid(a, b, c):
-    return (a[0]+b[0]+c[0])/3.0, (a[1]+b[1]+c[1])/3.0
+def area(A):
+    return (ED(A[0],A[1])*ED(A[1],(A[1][0],A[2][1])))/2.0
+
+#############################################################################
+#   FIND THE CENTROID OF TRIANGLE A
+
+def centroid(A):
+    return (A[0][0]+A[1][0]+A[2][0])/3.0,(A[0][1]+A[1][1]+A[2][1])/3.0
 
 #############################################################################
 #   CALCULATE THE EUCLIDEAN DISTANCE BETWEEN TWO POINTS
@@ -34,80 +34,64 @@ def HausdorffDistance(A, B):
 #   CALCULATE THE DIFFERENCE IN AREA BETWEEN TWO TRIANGLES
 
 def areaDistance(A, B):
-    area_A = area(A[0], A[1], A[2])
-    area_B = area(B[0], B[1], B[2])
-    return abs(area_A-area_B)
+    return abs(area(A)-area(B))
 
 #############################################################################
-# TRANSLATE THE COORDINATES FOR TRIANGLE B SO THAT ITS CENTROID OR ORIENTING
-# SPOT ALIGNS WITH THAT OF TRIANGLE A
+# TRANSLATE THE COORDINATES FOR TRIANGLE B SO THAT ITS CENTROID ALIGNS WITH
+# THAT OF TRIANGLE A
 
 def translate(A, B):
-    A_centroid = centroid(A[0], A[1], A[2])
-    B_centroid = centroid(B[0], B[1], B[2])
-    x_shift = A_centroid[0] - B_centroid[0]
-    y_shift = A_centroid[1] - B_centroid[1]
-    B1x = B[0][0] + x_shift
-    B1y = B[0][1] + y_shift
-    B2x = B[1][0] + x_shift
-    B2y = B[1][1] + y_shift
-    B3x = B[2][0] + x_shift
-    B3y = B[2][1] + y_shift
-    return [(B1x, B1y), (B2x, B2y), (B3x, B3y)]
+    A_c = centroid(A)
+    B_c = centroid(B)
+    x = A_c[0]-B_c[0]
+    y = A_c[1]-B_c[1]
+    return [(B[0][0]+x,B[0][1]+y),(B[1][0]+x,B[1][1]+y),(B[2][0]+x,B[2][1]+y)]
 
 #############################################################################
 # DETERMINE THE ROTATION OF TRIANGLE A IN RADIANS
 
-def rotation(A):
-    '''Determine the rotation of triangle A'''
-    A_c = centroid(A[0], A[1], A[2])
-    top_point = (A_c[0], 0)
-    a = ED(A_c, A[0])
-    b = ED(A_c, top_point)
-    c = ED(top_point, A[0])
-    ang = arccos(((a**2)+(b**2)-(c**2))/float(2*a*b))
+def determine_rotation(A):
+    A_c = centroid(A)
+    a = ED(A_c,A[0])
+    b = ED(A_c,(A_c[0],0))
+    c = ED((A_c[0],0),A[0])
+    angle = arccos(((a**2)+(b**2)-(c**2))/(2.0*a*b))
     if A[0][0] < A_c[0]:
-        ang = pi + (pi-ang)
-    return ang
+        angle = pi+(pi-angle)
+    return angle
 
 #############################################################################
-# ROTATE TRIANGLE A AROUND ITS CENTROID BY ang RADIANS
+# ROTATE TRIANGLE A AROUND ITS CENTROID BY r RADIANS
 
-def rotate(A, ang):
-    '''Rotate triangle A around its centroid by ang radians'''
-    pts = array([[A[0][0], A[0][1]], [A[1][0], A[1][1]], [A[2][0], A[2][1]]])
-    c = centroid(A[0], A[1], A[2])
-    cnt = array([c[0], c[1]])
-    return dot(pts-cnt,array([[cos(ang),sin(ang)],[-sin(ang),cos(ang)]]))+cnt
+def rot(A, r):
+    pts = array([[A[0][0],A[0][1]],[A[1][0],A[1][1]],[A[2][0],A[2][1]]])
+    A_c = centroid(A)
+    cnt = array([A_c[0],A_c[1]])
+    return dot(pts-cnt,array([[cos(r),sin(r)],[-sin(r),cos(r)]]))+cnt
 
 #############################################################################
 # ROTATE B SO THAT IT IS POINTING IN THE SAME DIRECTION AS A
 
-def align_rotations(A, B):
-    '''Rotate B so that it is pointing in the same direction as A'''
-    A_rotation = rotation(A)
-    B_rotation = rotation(B)
-    B_r = rotate(B, A_rotation-B_rotation)
-    B = [(B_r[0][0], B_r[0][1]), (B_r[1][0], B_r[1][1]), (B_r[2][0], B_r[2][1])]
-    return B
+def rotate(A, B):
+    A_r = determine_rotation(A)
+    B_r = determine_rotation(B)
+    B = rot(B, A_r-B_r)
+    return [(B[0][0],B[0][1]),(B[1][0],B[1][1]),(B[2][0],B[2][1])]
 
 #############################################################################
 # SCALE TRIANGLE A SO THAT ITS AREA IS 100,000 SQUARE PIXELS
 
 def scale(A):
-    ar = area(A[0], A[1], A[2])
+    ar = area(A)
     sf = sqrt(100000.0/float(ar))
-    A_c = centroid(A[0], A[1], A[2])
-    A = [((A[0][0]-A_c[0])*sf+A_c[0], (A[0][1]-A_c[1])*sf+A_c[1]),
-         ((A[1][0]-A_c[0])*sf+A_c[0], (A[1][1]-A_c[1])*sf+A_c[1]),
-         ((A[2][0]-A_c[0])*sf+A_c[0], (A[2][1]-A_c[1])*sf+A_c[1])]
-    return A
+    A_c = centroid(A)
+    return [((A[0][0]-A_c[0])*sf+A_c[0],(A[0][1]-A_c[1])*sf+A_c[1]),((A[1][0]-A_c[0])*sf+A_c[0],(A[1][1]-A_c[1])*sf+A_c[1]),((A[2][0]-A_c[0])*sf+A_c[0],(A[2][1]-A_c[1])*sf+A_c[1])]
 
 #############################################################################
 # TRANSLATE AND ROTATE B SO THAT IT IS LIKE A
 
 def rigid_motion(A, B):
-    return translate(A, align_rotations(A, B))
+    return translate(A, rotate(A, B))
 
 #############################################################################
 # TRIANGLE DISTANCE MEASURES
@@ -119,7 +103,7 @@ def dT_up_to_translation(A, B):
     return dT(A, translate(A, B))
 
 def dT_up_to_rotation(A, B):
-    return dT(A, align_rotations(A, B))
+    return dT(A, rotate(A, B))
 
 def dT_up_to_scale(A, B):
     return dT(scale(A), scale(B))
@@ -131,7 +115,7 @@ def dT_up_to_scaled_translation(A, B):
     return dT(scale(A), scale(translate(A, B)))
 
 def dT_up_to_scaled_rotation(A, B):
-    return dT(scale(A), scale(align_rotations(A, B)))
+    return dT(scale(A), scale(rotate(A, B)))
 
-def dT_up_to_isometry(A, B):
+def dT_up_to_scaled_rigid_motion(A, B):
     return dT(scale(A), scale(rigid_motion(A, B)))
