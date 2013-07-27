ccfc5141ba8cd3a8d4865f251a67f0f6af2f951c
Jon Carr <j.w.carr@icloud.com>
2013-07-27 22:37:57 +0100

    Now returns natual log of dT
    dT is now defined at the natural log of 1 + the sum of the Euclidean
    distances between points. This log transform avoids the problem where
    scaling super-skinny triangles results in a triangle that is crazy long
    and therefore extremely distant from another more normal triangle
    (which in turn skews the Pearson correlation).

--- geometry.py
+++ geometry.py
@@ -1,4 +1,4 @@
-from numpy import arccos, array, cos, dot, sin, sqrt
+from numpy import arccos, array, cos, dot, log, sin, sqrt
 
 #############################################################################
 #   CALCULATE THE EUCLIDEAN DISTANCE BETWEEN TWO POINTS
@@ -38,7 +38,7 @@ def rotate(A):
     return dot(A-c,array([[cos(theta),sin(theta)],[-sin(theta),cos(theta)]]))+c
 
 #############################################################################
-# SCALE TRIANGLE A SO THAT ITS AREA IS 18,00 SQUARE PIXELS
+# SCALE TRIANGLE A SO THAT ITS AREA IS 18,000 SQUARE PIXELS
 
 def scale(A):
     f = sqrt(18000.0/area(A))
@@ -49,7 +49,7 @@ def scale(A):
 # TRIANGLE DISTANCE MEASURES
 
 def dT(A, B):
-    return ED(A[0],B[0])+min(ED(A[1],B[1])+ED(A[2],B[2]),ED(A[1],B[2])+ED(A[2],B[1]))
+    return log(1+ED(A[0],B[0])+min(ED(A[1],B[1])+ED(A[2],B[2]),ED(A[1],B[2])+ED(A[2],B[1])))
 
 def dT_up_to_translation(A, B):
     return dT(A, translate(B, A))
