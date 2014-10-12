7cf57c76a5aad0009d2fe8b18bad9c2be4b1999b
Jon Carr <jwcarr@users.noreply.github.com>
2014-10-12 23:33:55 +0100

    Tidied up commentary

--- Experiment/php/main.php
+++ Experiment/php/main.php
@@ -15,17 +15,13 @@ $new_map = $parsed_map[0];
 $experiment_page = $parsed_map[1];
 $item_info = $parsed_map[2];
 
-$xy = array(0, 0, 0, 0, 0, 0);
-
 // Set window location for next page for use in JavaScript
 $window_location = "index.php?page=experiment&subject=". $_REQUEST["subject"] ."&cond=". $_REQUEST["cond"] ."&chain=". $_REQUEST["chain"] ."&gen=". $_REQUEST["gen"] ."&map=". $new_map;
 
+// If this is a BEGIN PAGE
 if ($experiment_page == "BEGIN") {
   // Clear the data files in case they already contain content
   clearFiles($_REQUEST["cond"], $_REQUEST["chain"], $_REQUEST["gen"]);
-
-  // Write the condition, chain, and generation number to the detect file for use by the status tracker
-  writeFile("detect", $_REQUEST["cond"] . "||" . $_REQUEST["chain"] . "||" . $_REQUEST["gen"]);
 }
 
 // If this is a TRAINING PAGE
@@ -50,7 +46,7 @@ elseif ($experiment_page == "TR") {
 
 }
 
-// If this is a MINI TEST PAGE
+// If this is a MINI-TEST PAGE
 elseif ($experiment_page == "MT") {
   // Get the XY coordinates for for the random chosen stimulus from the previous participant's dynamic set file
   $xy = loadTriangle($_REQUEST["cond"], $_REQUEST["chain"], ($_REQUEST["gen"]-1), "d", $item_info);
@@ -58,9 +54,9 @@ elseif ($experiment_page == "MT") {
   $correct_answer = getWord($_REQUEST["cond"], $_REQUEST["chain"], $_REQUEST["gen"]-1, $item_info);
 }
 
-// If this is a TEST PAGE
+// If this is a TEST PAGE or DIRECTOR PAGE
 elseif ($experiment_page == "TS" OR $experiment_page == "DR") {
-  // Parse the test item information into its set (either "d" or "s") and the stimulus number from that set (number between 0 and 47)
+  // Parse the test item information into its set (either "d" or "s") and the stimulus number from that set
   $stimulus_info = explode(".", $item_info);
   $stimulus_set = $stimulus_info[0];
   $stimulus_number = $stimulus_info[1];
@@ -111,35 +107,34 @@ elseif ($experiment_page == "TS" OR $experiment_page == "DR") {
     // Save the previous answer to the relevant file
     saveAnswer($_REQUEST["cond"], $_REQUEST["chain"], $_REQUEST["gen"], $_REQUEST["current"], $_REQUEST["a"], $last_xy);
   }
-  
+
 }
 
+// If this is a MATCHER PAGE
 elseif ($experiment_page == "MR") {
-
+  // Parse the test item information into its set (either "d" or "s") and the stimulus number from that set
   $stimulus_info = explode(".", $item_info);
   $stimulus_set = $stimulus_info[0];
   $stimulus_number = $stimulus_info[1];
 
+  // Generate a set of random distractor triangles for the matcher array
   $triangle_array = generateTriangleArray();
 
+  // For each distractor triangle
   foreach ($triangle_array as $triangle) {
-    $triangle_array_JS .= "\"";
-    $triangle_array_JS .= implode("\", \"", $triangle);
-    $triangle_array_JS .= "\", ";
+    // add its coordinates to the
+    $triangle_array_JS .= implode(",", $triangle);
   }
-
-  $target = $triangle_array[1];
 
 }
 
+// If this is a BREAK PAGE or WAIT PAGE
 elseif ($experiment_page == "BREAK" OR $experiment_page == "WAIT") {
-  // If an answer to a mini test has been provided...
-  if ($_REQUEST["a"] != "") {
-    // Write the answer to the log
-    saveLogData($_REQUEST["correct_answer"] ."\t". $_REQUEST["a"]);
-  }
+  // Write the final mini-test answer to the log
+  saveLogData($_REQUEST["correct_answer"] ."\t". $_REQUEST["a"]);
 }
 
+// If this is an END PAGE
 elseif ($experiment_page == "END") {
   // Create an XY array from the previous XY coordinates
   $last_xy = array($_REQUEST["last_x1"], $_REQUEST["last_x2"], $_REQUEST["last_x3"], $_REQUEST["last_y1"], $_REQUEST["last_y2"], $_REQUEST["last_y3"]);
@@ -147,12 +142,12 @@ elseif ($experiment_page == "END") {
   // Save the final answer (and sort the stable data file back to its unshuffled order)
   saveFinalAnswer($_REQUEST["cond"], $_REQUEST["chain"], $_REQUEST["gen"], $_REQUEST["current"], $_REQUEST["a"], $last_xy);
 
-  // Write time at whcih the experiment ended to log
-  if ($_REQUEST["cond"] == 1) {
-    saveLogData("\nEND AT " . date("d/m/Y H:i:s") . "\n-------------------------------------------------------\n\n");
+  // Write time at whcih the experiment ended to log along with overuse count if condition 2
+  if ($_REQUEST["cond"] == 2) {
+    saveLogData("\nOveruse count = ". $_REQUEST["overuse"] ."\n\nEND AT " . date("d/m/Y H:i:s") . "\n-------------------------------------------------------\n\n");
   }
   else {
-    saveLogData("\nOveruse count = ". $_REQUEST["overuse"] ."\n\nEND AT " . date("d/m/Y H:i:s") . "\n-------------------------------------------------------\n\n");
+    saveLogData("\nEND AT " . date("d/m/Y H:i:s") . "\n-------------------------------------------------------\n\n");
   }
 }
 
