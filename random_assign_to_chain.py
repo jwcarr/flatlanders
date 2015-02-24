2ba173e9d9bf8a626b0df220dc8b6ece66113452
Jon Carr <jwcarr@users.noreply.github.com>
2015-02-24 22:17:47 +0000

    Bug fixes

--- random_assign_to_chain.py
+++ random_assign_to_chain.py
@@ -2,12 +2,12 @@
 
 from random import randrange
 
-def assign(chain_codes, generations):
+def assign(chain_codes, start_gen, end_gen):
   chain_gens = {}
   for chain in chain_codes:
-    chain_gens[chain] = range(1, generations+1)
+    chain_gens[chain] = range(start_gen, end_gen+1)
   random_assignment = []
-  for i in range(0, len(chain_codes)*generations):
+  for i in range(0, len(chain_codes)*(end_gen-start_gen+1)):
     code = chain_codes[randrange(0, len(chain_codes))]
     gen = chain_gens[code][0]
     del chain_gens[code][0]
