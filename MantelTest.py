92ddb5ebd399c194c5d0562cf99b1347a4b93709
Jon Carr <jwcarr@users.noreply.github.com>
2015-02-24 22:20:43 +0000

    Various changes taken in from MantelTest repo

--- MantelTest.py
+++ MantelTest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from scipy import arange, array, corrcoef, mean, random, spatial, stats, std, zeros
+from scipy import arange, array, mean, random, spatial, stats, std, zeros
 
 
 
@@ -11,15 +11,29 @@ from scipy import arange, array, corrcoef, mean, random, spatial, stats, std, ze
 #   significance of the veridical correlation, and a p-value for a normality
 #   test on the distribution of sample correlations (norm).
 
-def MantelTest(distances1, distances2, randomizations=10000):
-  if ValidateInput(distances1, distances2, randomizations) == False:
-    return None
+def MantelTest(distances1, distances2, randomizations=10000, correlation_measure='pearson'):
+  ValidateInput(distances1, distances2, randomizations, correlation_measure)
   vector1 = array(distances1, dtype=float)
   vector2 = array(distances2, dtype=float)
-  r = corrcoef([vector1, vector2])[0, 1]
-  m, sd, norm = MonteCarlo(vector1, vector2, randomizations)
+  r, p = Correlate(vector1, vector2, correlation_measure)
+  m, sd, norm = MonteCarlo(vector1, vector2, randomizations, correlation_measure)
   z = (r-m)/sd
-  return r, m, sd, z, norm
+  return r, p, m, sd, z, norm
+
+
+
+# Correlate()
+#   Correlates two vectors using a given type of correlation coefficient
+
+def Correlate(vector1, vector2, correlation_measure):
+  if correlation_measure == 'pearson':
+    return stats.pearsonr(vector1, vector2)
+  elif correlation_measure == 'spearman':
+    return stats.spearmanr(vector1, vector2)
+  elif correlation_measure == 'kendall':
+    return stats.kendalltau(vector1, vector2)
+  else:
+    raise ValueError('The correlation_measure should be set to "pearson", "spearman", or "kendall"')
 
 
 
@@ -29,10 +43,10 @@ def MantelTest(distances1, distances2, randomizations=10000):
 #   standard deviation of the correlations, and a p-value for a normality test
 #   of the distribution of correlations.
 
-def MonteCarlo(vector1, vector2, randomizations):
+def MonteCarlo(vector1, vector2, randomizations, correlation_measure):
   correlations = zeros(randomizations, dtype=float)
   for i in xrange(0, randomizations):
-    correlations[i] = corrcoef([vector1, MatrixShuffle(vector2)])[0, 1]
+    correlations[i] = Correlate(vector1, MatrixShuffle(vector2), correlation_measure)[0]
   return mean(correlations), std(correlations), stats.normaltest(correlations)[1]
 
 
@@ -57,21 +71,18 @@ def MatrixShuffle(vector):
 
 
 # ValidateInput()
-#   Validates input arguments and returns an error message if a problem is
-#   identified. Returns True otherwise.
+#   Validates input arguments and raises an error if a problem is identified.
 
-def ValidateInput(distances1, distances2, randomizations):
-  if type(randomizations) == int:
-    if type(distances1) == list and type(distances2) == list:
-      if len(distances1) == len(distances2):
-        if spatial.distance.is_valid_y(array(distances1, dtype=float)) == True:
-          if spatial.distance.is_valid_y(array(distances2, dtype=float)) == True:
-            return True
-        print('Error: the sets of pairwise distances are invalid')
-        return False
-      print('Error: the sets of distances should be of the same length')
-      return False
-    print('Error: the sets of distances should be lists')
-    return False
-  print('Error: the number of randomizations should be an integer')
-  return False
+def ValidateInput(distances1, distances2, randomizations, correlation_measure):
+  if type(randomizations) != int:
+    raise ValueError('The number of randomizations should be an integer')
+  if type(correlation_measure) != str or correlation_measure not in ['pearson', 'spearman', 'kendall']:
+    raise ValueError('The correlation_measure should be set to "pearson", "spearman", or "kendall"')
+  if type(distances1) != list or type(distances2) != list:
+    raise ValueError('The sets of pairise distances should be Python lists')
+  if len(distances1) != len(distances2):
+    raise ValueError('The two sets of pairwise distances should be of the same length')
+  if spatial.distance.is_valid_y(array(distances1, dtype=float)) == False:
+    raise ValueError('The first set of pairwise distances is invalid')
+  if spatial.distance.is_valid_y(array(distances2, dtype=float)) == False:
+    raise ValueError('The second set of pairwise distances is invalid')
