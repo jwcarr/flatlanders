20119d00a2b2aa725b9553d4d35389cf510c745b
Jon Carr <jwcarr@users.noreply.github.com>
2015-07-05 21:46:45 +0100

    Updated Page's test code

--- page.py
+++ page.py
@@ -1,102 +1,125 @@
-'''
-A Python implementation of Page's trend test (Page, 1963)
-Written by Jon W. Carr
-
-Takes a matrix, with treatments along the columns and replications along
-the rows, and returns Page's (1963) L statistic, along with its p-value.
-
-The a-priori hypothesis for directionality must be specified - either
-"ascending" or "descending" - depending on the trend that is hypothesized.
-
-Example:
-
-data = [[100,90,105,70,5],
-        [200,150,80,50,10],
-        [121,130,75,20,25],
-        [90,75,76,54,32]]
-ptt(data, "descending")
-
-This will return the 4-tuple: (l, m, n, p), where l = Page's L statistic,
-m = number of replications, n = number of treatments, and p = the p-value.
-
-Reference:
-Page, E. (1963). Ordered hypotheses for multiple treatments: A significance
-    test for linear ranks. Journal of the American Statistical Association,
-    58, 216-230.
-'''
-
 from scipy import stats
+import numpy
 
-critical_values = [[],[],[],[[],[],[28],[41,42],[54,55,56],[66,68,70],[79,81,83],[91,93,96],[104,106,109],[116,119,121],[128,131,134],[141,144,147],[153,156,160],[165,169,172],[178,181,185],[190,194,197],[202,206,210],[215,218,223],[227,231,235],[239,243,248],[251,256,260]],[[],[],[58,60],[84,87,89],[111,114,117],[137,141,145],[163,167,172],[189,193,198],[214,220,225],[240,246,252],[266,272,278],[292,298,305],[317,324,331]],[[],[],[103,106,109],[150,155,160],[197,204,210],[244,251,259],[291,299,307],[338,346,355],[384,393,403],[431,441,451],[477,487,499],[523,534,546],[570,581,593]],[[],[],[166,173,178],[244,252,260],[321,331,341],[397,409,420],[474,486,499],[550,563,577],[625,640,655],[701,717,733],[777,793,811],[852,869,888],[928,946,965]],[[],[],[252,261,269],[370,382,394],[487,501,516],[603,620,637],[719,737,757],[835,855,876],[950,972,994],[1065,1088,1113],[1180,1205,1230],[1295,1321,1348],[1410,1437,1465]],[[],[],[362,376,388],[532,549,567],[701,722,743],[869,893,917],[1037,1063,1090],[1204,1232,1262],[1371,1401,1433],[1537,1569,1603],[1703,1736,1773],[1868,1905,1943],[2035,2072,2112]]]
 
-def ptt(matrix, hypothesis):
+# Test()
+#   Run Page's test and return l, m, n, p, where l = Page's L statistic,
+#   m = number of replications, n = number of treatments, and p = p-value.
+
+def Test(matrix, ascending=False, use_critical_values=False):
+    ValidateInput(matrix, ascending, use_critical_values)
+    if ascending == True:
+        matrix = MirrorMatrix(matrix)
     m = len(matrix)
     n = len(matrix[0])
-    if n < 3:
-        print("Page's trend test requires at least 3 treatments.")
-        return
-    if m < 2:
-        print("Page's trend test requires at least 2 replications.")
-        return
-    if hypothesis == "ascending" or hypothesis == "a":
-        ordered_matrix = mirror_matrix(matrix)
-    elif hypothesis == "descending" or hypothesis == "d":
-        ordered_matrix = matrix
-    else:
-        print("Invalid hypothesis. Hypothesis should be set to \"ascending\" or \"descending\".")
-        return
-    l = page_l(ordered_matrix, m, n)
-    if n == 3 and m < 21: p = page_critical_p(l, m, n)
-    elif n > 3 and n < 9 and m < 13: p = page_critical_p(l, m, n)
-    else: p = page_exact_p(l, m, n, ordered_matrix)
+    l = PageL(matrix, m, n)
+    p = PageP(l, m, n, matrix, use_critical_values)
     return l, m, n, p
 
-def page_l(matrix, m, n):
+
+
+# PageL()
+#   Calculate Page's L statistic.
+
+def PageL(matrix, m, n):
     rank_matrix = []
-    for i in range(0,m):
+    for i in range(0, m):
         rank = stats.rankdata(matrix[i])
         rank_list = []
-        for j in range(0,n):
+        for j in range(0, n):
             rank_list.append(rank[n-j-1])
         rank_matrix.append(rank_list)
     ranks = []
-    for i in range(0,n):
+    for i in range(0, n):
         total = sum([row[i] for row in rank_matrix])
-        total = total * (i+1)
+        total *= i + 1
         ranks.append(total)
-    l = sum(ranks)
-    return l
+    return sum(ranks)
 
-def page_critical_p(l, m, n):
-    values = critical_values[n][m]
-    level = None
-    for i in range(len(values)):
-        if l >= values[i]:
-            level = i
-    if level == 0: return "< 0.05"
-    elif level == 1: return "< 0.01"
-    elif level == 2: return "< 0.001"
-    else: return "= n.s."
 
-def page_exact_p(l, m, n, matrix):
-    alt_l = page_l(mirror_matrix(matrix), m, n)
+
+# PageP()
+#   Calculate a p-value for L using the appropriate method.
+
+def PageP(l, m, n, matrix, use_critical_values):
+    if use_critical_values == True:
+        try:
+            return PageCriticalP(l, m, n)
+        except IndexError:
+            print('Large data matrix, so calculating exact p-value instead')
+    return PageExactP(l, m, n, matrix)
+
+
+
+# PageCriticalP()
+#   For small m and n, the exact p-value won't always agree with the critical
+#   values given in Page (1963, p. 220). If you prefer, you can use Page's critical
+#   values instead. This function looks up the critical values for m and n, and
+#   finds the significance level for L.
+
+def PageCriticalP(l, m, n):
+    values = critical_values[n-3][m-2]
+    significance_levels = ['< 0.001', '< 0.01', '< 0.05']
+    for i in range(0, 3):
+        if l >= values[i] and values[i] != None:
+            return significance_levels[i]
+    return 'n.s.'
+
+
+
+# PageExactP()
+#   Calculate the exact p-value using Eqation 4 in Page (1963)
+
+def PageExactP(l, m, n, matrix):
+    # Calcualte L for the opposite trend
+    alt_l = PageL(MirrorMatrix(matrix), m, n)
+    # If L for the opposite trend > L for the hypothesized trend, then the trend
+    # can't be significant ...
     if alt_l > l:
-        return "= n.s."
-    else:
-        chi_squared = ((12.0*l-3.0*m*n*(n+1.0)**2.0)**2.0)/(m*n**2.0*(n**2.0-1.0)*(n+1.0))
-        p_two_tailed = 1 - stats.chi2.cdf(chi_squared, 1)
-        p_one_tailed = p_two_tailed / 2.0
-    return "= %s" % p_one_tailed
+        # ... so return 'n.s.', otherwise the exact p-value could be misleading
+        # if the opposite trend happens to be significant
+        return 'n.s.'
+    chi_squared = ((12.0*l-3.0*m*n*(n+1.0)**2.0)**2.0)/(m*n**2.0*(n**2.0-1.0)*(n+1.0))
+    p_two_tailed = 1 - stats.chi2.cdf(chi_squared, 1)
+    # Return one-tailed p-value, since this is a one-tailed test
+    return p_two_tailed / 2.0
 
-def mirror_matrix(matrix):
-    m = len(matrix)
-    n = len(matrix[0])
-    columns = []
-    for i in range(0,n):
-        columns.append([row[i] for row in matrix])
-    mirrored_matrix = []
-    for i in range(0,m):
-        new_row = [row[i] for row in columns]
-        new_row.reverse()
-        mirrored_matrix.append(new_row)
-    return mirrored_matrix
+
+
+# MirrorMatrix()
+#   Reverses the columns of a matrix
+
+def MirrorMatrix(matrix):
+    return [[row[i] for i in reversed(range(len(matrix[0])))] for row in matrix]
+
+
+
+# ValidateInput()
+#   Validates the input arguments to catch common problems
+
+def ValidateInput(matrix, ascending, use_critical_values):
+    if type(matrix) != list:
+        raise TypeError('Matrix should be represented as Python lists')
+    for row_type in [type(row) for row in matrix]:
+        if row_type != list:
+            raise TypeError('Rows of the matrix should be represented as Python lists')
+    for cell_type in [type(matrix[i][j]) for i in range(len(matrix)) for j in range(len(matrix[i]))]:
+        if cell_type not in (int, float, numpy.float64):
+            print cell_type
+            raise TypeError('Matrix should contain integers or floating points')
+    if len(set([len(row) for row in matrix])) != 1:
+        raise ValueError('Rows in matrix should have same length')
+    if len(matrix) < 2:
+        raise ValueError('Page\'s test requires at least 2 replications')
+    if len(matrix[0]) < 3:
+        raise ValueError('Page\'s test requires at least 3 treatments')
+    if type(ascending) != bool:
+        raise TypeError('The ascending argument should be set to True or False')
+    if type(use_critical_values) != bool:
+        raise TypeError('The use_critical_values argument should be set to True or False')
+
+
+
+# Critical values from Page (1963) for small m and n
+
+critical_values = [[[None, None, 28], [None, 42, 41], [56, 55, 54], [70, 68, 66], [83, 81, 79], [96, 93, 91], [109, 106, 104], [121, 119, 116], [134, 131, 128], [147, 144, 141], [160, 156, 153], [172, 169, 165], [185, 181, 178], [197, 194, 190], [210, 206, 202], [223, 218, 215], [235, 231, 227], [248, 243, 239], [260, 256, 251]], [[None, 60, 58], [89, 87, 84], [117, 114, 111], [145, 141, 137], [172, 167, 163], [198, 193, 189], [225, 220, 214], [252, 246, 240], [278, 272, 266], [305, 298, 292], [331, 324, 317]], [[109, 106, 103], [160, 155, 150], [210, 204, 197], [259, 251, 244], [307, 299, 291], [355, 346, 338], [403, 393, 384], [451, 441, 431], [499, 487, 477], [546, 534, 523], [593, 581, 570]], [[178, 173, 166], [260, 252, 244], [341, 331, 321], [420, 409, 397], [499, 486, 474], [577, 563, 550], [655, 640, 625], [733, 717, 701], [811, 793, 777], [888, 869, 852], [965, 946, 928]], [[269, 261, 252], [394, 382, 370], [516, 501, 487], [637, 620, 603], [757, 737, 719], [876, 855, 835], [994, 972, 950], [1113, 1088, 1065], [1230, 1205, 1180], [1348, 1321, 1295], [1465, 1437, 1410]], [[388, 376, 362], [567, 549, 532], [743, 722, 701], [917, 893, 869], [1090, 1063, 1037], [1262, 1232, 1204], [1433, 1401, 1371], [1603, 1569, 1537], [1773, 1736, 1703], [1943, 1905, 1868], [2112, 2072, 2035]]]
