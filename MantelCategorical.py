1fee53ff3f6bdef89260fdb1c0b651b0bf8053a1
Jon Carr <jwcarr@users.noreply.github.com>
2015-07-26 22:18:36 +0100

    Added deterministic version for faster computation
    and stopped using dictionaries because I suspect this may have been
    causing issues with the randomness of the shuffle (rating to
    dictionaries having no order)

--- MantelCategorical.py
+++ MantelCategorical.py
@@ -1,39 +1,82 @@
-from scipy import array, random, stats, zeros
+from itertools import permutations
+from math import factorial
+from numpy import array, corrcoef, random, zeros
 import basics
 import rater_analysis
 
-def MantelTest(experiment, chain, generation, set_type, randomizations):
+# Run a categorical Mantel test. This is like a regular Mantel test, except that you're
+# shuffling the categories that meanings belong to rather than shuffling the string-meaning
+# mapping freely.
+
+def Test(experiment, chain, generation, set_type, randomizations):
   words = basics.getWords(experiment, chain, generation, set_type)
   word_distances = basics.stringDistances(words)
-  categorized_words = CategorizeWords(words)
+  categorized_words, categorized_meanings = CategorizeWords(words)
   meaning_distances = rater_analysis.reliable_distance_array
-  r, p = stats.pearsonr(meaning_distances, word_distances)
-  m, sd = MonteCarlo(meaning_distances, categorized_words, randomizations)
+  r = corrcoef(meaning_distances, word_distances)[0,1]
+  n = len(words)
+  m, sd = Mantel(meaning_distances, categorized_words, categorized_meanings, r, n, generation, randomizations)
   z = (r-m)/sd
-  return r, p, m, sd, z
+  return r, m, sd, z
+
+# Takes a list of strings and returns two lists: a list of the unique strings, and another
+# list of the same length which gives the meaning indices that the words map on to. Not using
+# a dictionary because this can potentially create issues with the shuffling because
+# dictionaries don't have order.
 
 def CategorizeWords(words):
-  categorized_words = {}
+  categorized_words = []
+  categorized_meanings = []
   for i in xrange(len(words)):
-    if words[i] in categorized_words.keys():
-      categorized_words[words[i]].append(i)
-    else:
-      categorized_words[words[i]] = [i]
-  return categorized_words
+    try:
+      position = categorized_words.index(words[i])
+      categorized_meanings[position].append(i)
+    except ValueError:
+      categorized_words.append(words[i])
+      categorized_meanings.append([i])
+  return categorized_words, categorized_meanings
 
-def MonteCarlo(meaning_distances, categorized_words, randomizations):
+# If the number of possible mappings is less than the randomizations parameter, conduct a
+# deterministic Mantel test; otherwise, do a stochasitc Mantel test.
+
+def Mantel(meaning_distances, categorized_words, categorized_meanings, r, n, generation, randomizations):
+  if factorial(len(categorized_words)) <= randomizations:
+    print '    Generation %d: Only %d possible permutations - computing result deterministically' % (generation, factorial(len(categorized_words)))
+    return DeterministicMantel(meaning_distances, categorized_words, categorized_meanings, n)
+  return StochasticMantel(meaning_distances, categorized_words, categorized_meanings, r, n, randomizations)
+
+# Stochasitc Mantel test - randomly sample the space of category-meaning mappings
+
+def StochasticMantel(meaning_distances, categorized_words, categorized_meanings, r, n, randomizations):
   correlations = zeros(randomizations, dtype=float)
-  for i in xrange(randomizations):
-    correlations[i] = stats.pearsonr(meaning_distances, GeneratePermutation(categorized_words))[0]
+  correlations[0] = r
+  for i in xrange(1, randomizations):
+    correlations[i] = corrcoef(meaning_distances, GeneratePermutation(categorized_words, categorized_meanings, n))[0,1]
   return correlations.mean(), correlations.std()
 
-def GeneratePermutation(categorized_words):
-  words = categorized_words.keys()
-  meanings = categorized_words.values()
-  random.shuffle(words)
-  permuted_categorization = dict(zip(words, meanings))
-  permuted_words = [None] * 48
-  for word in words:
-    for meaning in permuted_categorization[word]:
-      permuted_words[meaning] = word
+def GeneratePermutation(categorized_words, categorized_meanings, n):
+  random.shuffle(categorized_words)
+  permuted_words = [None] * n
+  for i in range(len(categorized_meanings)):
+    for j in categorized_meanings[i]:
+      permuted_words[j] = categorized_words[i]
+  return basics.stringDistances(permuted_words)
+
+# Deterministic Mantel test - measure every possible category-meaning mapping
+
+def DeterministicMantel(meaning_distances, categorized_words, categorized_meanings, n):
+  m = len(categorized_words)
+  correlations = zeros(factorial(m), dtype=float)
+  orders = permutations(range(m))
+  i = 0
+  for order in orders:
+    correlations[i] = corrcoef(meaning_distances, GenerateDeterministicPermutation(categorized_words, categorized_meanings, order, n))[0,1]
+    i += 1
+  return correlations.mean(), correlations.std()
+
+def GenerateDeterministicPermutation(categorized_words, categorized_meanings, order, n):
+  permuted_words = [None] * n
+  for i in range(len(categorized_meanings)):
+    for j in categorized_meanings[i]:
+      permuted_words[j] = categorized_words[order[i]]
   return basics.stringDistances(permuted_words)
