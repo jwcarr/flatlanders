668c6ecb31604d87a8e124cc255cffd07aa12a7e
Jon Carr <jwcarr@users.noreply.github.com>
2015-08-02 11:07:46 +0100

    Removed old distance measures

--- geometry.py
+++ geometry.py
@@ -173,30 +173,3 @@ def dist_to_nearest_edge(A, vertex):
         if dist < smallest_dist:
             smallest_dist = dist
     return smallest_dist
-
-#############################################################################
-# TRIANGLE DISTANCE MEASURES
-
-def dT(A, B):
-    return ED(A[0],B[0])+min(ED(A[1],B[1])+ED(A[2],B[2]),ED(A[1],B[2])+ED(A[2],B[1]))
-
-def dT_up_to_translation(A, B):
-    return dT(A, translate(B, A))
-
-def dT_up_to_rotation(A, B):
-    return dT(rotate(A), rotate(B))
-
-def dT_up_to_scale(A, B):
-    return dT(scale(A), scale(B))
-
-def dT_up_to_rigid_motion(A, B):
-    return dT(rotate(A), translate(rotate(B), A))
-
-def dT_up_to_scaled_translation(A, B):
-    return dT(scale(A), scale(translate(B, A)))
-
-def dT_up_to_scaled_rotation(A, B):
-    return dT(scale(rotate(A)), scale(rotate(B)))
-
-def dT_up_to_scaled_rigid_motion(A, B):
-    return dT(scale(rotate(A)), scale(translate(rotate(B), A)))
