ed58fc5f59d45ffd7a45339422e9137a3b854971
Jon Carr <jwcarr@users.noreply.github.com>
2015-08-24 23:48:03 +0100

    Removed _dist from function names

--- geometrical_distance.py
+++ geometrical_distance.py
@@ -35,58 +35,58 @@ def triangle_distance_matrix(experiment, chain, generation, set_type, distance_m
 
 # Distance metrics
 
-def sizArea_dist(t1, t2):
+def sizArea(t1, t2):
   return abs(geometry.area(t1) - geometry.area(t2))
 
-def sizPeri_dist(t1, t2):
+def sizPeri(t1, t2):
   return abs(geometry.perimeter(t1) - geometry.perimeter(t2))
 
-def sizCent_dist(t1, t2):
+def sizCent(t1, t2):
   return abs(geometry.centroid_size(t1) - geometry.centroid_size(t2))
 
-def locOriX_dist(t1, t2):
+def locOriX(t1, t2):
   return abs(t1[0][0] - t2[0][0])
 
-def locOriY_dist(t1, t2):
+def locOriY(t1, t2):
   return abs(t1[0][1] - t2[0][1])
 
-def locCntX_dist(t1, t2):
+def locCntX(t1, t2):
   return abs(geometry.centroid(t1)[0] - geometry.centroid(t2)[0])
 
-def locCntY_dist(t1, t2):
+def locCntY(t1, t2):
   return abs(geometry.centroid(t1)[1] - geometry.centroid(t2)[1])
 
-def rotOri_dist(t1, t2):
+def rotOri(t1, t2):
   t1_rot = geometry.rotation(t1)
   t2_rot = geometry.rotation(t2)
-  return smallest_rot_dist(t1_rot, t2_rot)
+  return smallest_rot(t1_rot, t2_rot)
 
-def rotThin_dist(t1, t2):
+def rotThin(t1, t2):
   t1_rot = geometry.rotation_by_smallest_angle(t1)
   t2_rot = geometry.rotation_by_smallest_angle(t2)
-  return smallest_rot_dist(t1_rot, t2_rot)
+  return smallest_rot(t1_rot, t2_rot)
 
-def shpThin_dist(t1, t2):
+def shpThin(t1, t2):
   return abs(geometry.smallest_angle(t1) - geometry.smallest_angle(t2))
 
-def shpWide_dist(t1, t2):
+def shpWide(t1, t2):
   return abs(geometry.largest_angle(t1) - geometry.largest_angle(t2))
 
-def shpEqui_dist(t1, t2):
+def shpEqui(t1, t2):
   return abs(geometry.equilateralness(t1) - geometry.equilateralness(t2))
 
-def boxOriC_dist(t1, t2):
+def boxOriC(t1, t2):
   return abs(geometry.dist_to_nearest_corner(t1,0) - geometry.dist_to_nearest_corner(t2,0))
 
-def boxOriE_dist(t1, t2):
+def boxOriE(t1, t2):
   return abs(geometry.dist_to_nearest_edge(t1,0) - geometry.dist_to_nearest_edge(t2,0))
 
-def boxVrtC_dist(t1, t2):
+def boxVrtC(t1, t2):
   dists1 = [geometry.dist_to_nearest_corner(t1,i) for i in range(0,3)]
   dists2 = [geometry.dist_to_nearest_corner(t2,i) for i in range(0,3)]
   return abs(np.mean(dists1) - np.mean(dists2))
 
-def boxVrtE_dist(t1, t2):
+def boxVrtE(t1, t2):
   dists1 = [geometry.dist_to_nearest_edge(t1,i) for i in range(0,3)]
   dists2 = [geometry.dist_to_nearest_edge(t2,i) for i in range(0,3)]
   return abs(np.mean(dists1) - np.mean(dists2))
@@ -95,7 +95,7 @@ def boxVrtE_dist(t1, t2):
 
 # Given two rotations from Noth, return the shortest rotational distance
 
-def smallest_rot_dist(t1_rot, t2_rot):
+def smallest_rot(t1_rot, t2_rot):
   # If both rotations have the same sign...
   if t1_rot * t2_rot > 0:
     # ... just return the absolute difference
@@ -111,6 +111,6 @@ def smallest_rot_dist(t1_rot, t2_rot):
 
 ########################################
 
-metrics = [sizArea_dist, sizPeri_dist, sizCent_dist, locOriX_dist, locOriY_dist, locCntX_dist, locCntY_dist, rotOri_dist, rotThin_dist, shpThin_dist, shpWide_dist, shpEqui_dist, boxOriC_dist, boxOriE_dist, boxVrtC_dist, boxVrtE_dist]
-static_set_matrix = triangle_distance_matrix(1, "A", 0, "s", metrics)
+metrics = [sizArea, sizPeri, sizCent, locOriX, locOriY, locCntX, locCntY, rotOri, rotThin, shpThin, shpWide, shpEqui, boxOriC, boxOriE, boxVrtC, boxVrtE]
+static_set_matrix = triangle_distance_matrix(1, 'A', 0, 's', metrics)
 static_set_array = squareform(static_set_matrix, 'tovector')
