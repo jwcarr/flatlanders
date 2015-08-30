ad4d68988c1f8766466e105e643b980f059e6b0e
Jon Carr <jwcarr@users.noreply.github.com>
2015-08-31 00:26:36 +0100

    Simplified method to four features each with a single metric
    Also converting to ranks. Using condensed distance matrices and
    combining them in speedier way

--- geometrical_distance.py
+++ geometrical_distance.py
@@ -1,144 +1,62 @@
 from itertools import combinations
 from scipy.spatial.distance import squareform
+from scipy.stats import rankdata
 import numpy as np
 import basics
 import geometry
 
 ########################################
 
-# Generate a normalized distance matrix for a given geometrical metric
+# Generate a rank distance matrix for a given geometrical metric
 
-def property_distance_matrix(triangles, distance_metric):
-  n = len(triangles) # Number of triangles
-  # Create an empty distance matrix...
-  distance_matrix = np.zeros([n, n], dtype=float)
-  # ... and fill it in using the given metric
+def feature_matrix(triangles, distance_metric):
+  n = len(triangles)
+  # Store the distance between each pair of triangles
+  distance_array = []
   for i in range(0, n):
     for j in range(i+1, n):
-      dist = distance_metric(triangles[i], triangles[j])
-      distance_matrix[i, j] = dist
-      distance_matrix[j, i] = dist
-  # Normalize simply by dividing by max value, since distance matrices will always have a min of 0
-  return distance_matrix / distance_matrix.max()
+      distance_array.append(distance_metric(triangles[i], triangles[j]))
+  # Convert to ranks to remove distributional effects of each metric
+  return rankdata(distance_array)
 
-# Generate a composite distance matrix by combining properties into feature matrices
-# and averaging the feature matrices together
+# Generate a set of feature matrices given a set of geometrical metrics
 
-def composite_distance_matrix(triangles, distance_metrics):
-  n = len(triangles) # Number of triangles
-  feature_matrices = []
-  # For each feature...
-  for feature in distance_metrics:
-    # create an empty feature distance matrix
-    feature_matrix = np.zeros([n, n], dtype=float)
-    # For each metric within that feature...
-    for metric in feature:
-      # add on the property distance matrix
-      feature_matrix += property_distance_matrix(triangles, metric)
-    # Divide by the number of metrics to get the mean and store in feature_matrices
-    feature_matrices.append(feature_matrix / len(feature))
-  # Averge the feature matrices together for a composite distance matrix
-  return np.mean(feature_matrices, axis=0)
+def feature_matrices(triangles, distance_metrics):
+  matrices = []
+  # For each distance metric (i.e. each feature)...
+  for metric in distance_metrics:
+    # Compute and store the distance matrix
+    matrices.append(feature_matrix(triangles, metric))
+  return matrices
 
-# Enumerate all combinations of given features and create a composite distance
-# matrix for each combination
+# Enumerate all combinations of matrices and sum them together
 
-def combination_matrices(triangles, distance_metrics):
-  n = len(distance_metrics) # Number of features
+def combination_matrices(matrices):
+  n = len(matrices) # Number of features
   combin_matrices = []
   # For i = 1 ... n
   for i in range(1, n+1):
     # For each combination in "n choose i"...
     for combination_indices in combinations(range(n), i):
-      # Create a list of metrics to use
-      combin_metrics = [distance_metrics[j] for j in combination_indices]
-      # Create the composite distance matrix for that combination of metrics and store it
-      combin_matrices.append(composite_distance_matrix(triangles, combin_metrics))
+      # Sum the relevant matrices together for a composite distance matrix
+      sum_matrix = np.sum([matrices[j] for j in combination_indices], axis=0)
+      # Normalize and store the composite matrix
+      combin_matrices.append(sum_matrix / sum_matrix.max())
   # Return all the combination matrices
   return combin_matrices
 
 ########################################
 
-# Geometrical distance metrics: Each returns the absolute difference between two triangles
+# Euclidean distance between centroids
 
-# SIZE: Area
-def sizArea(t1, t2):
-  return abs(geometry.area(t1) - geometry.area(t2))
+def location_distance(t1, t2):
+  return geometry.ED(geometry.centroid(t1), geometry.centroid(t2))
 
-# Size: Perimeter
-def sizPeri(t1, t2):
-  return abs(geometry.perimeter(t1) - geometry.perimeter(t2))
+# Shortest radial distance by orienting spot
 
-# SIZE: Centroid size
-def sizCent(t1, t2):
-  return abs(geometry.centroid_size(t1) - geometry.centroid_size(t2))
-
-# LOCATION: Orienting spot on x-axis
-def locOriX(t1, t2):
-  return abs(t1[0][0] - t2[0][0])
-
-# LOCATION: Orienting spot on y-axis
-def locOriY(t1, t2):
-  return abs(t1[0][1] - t2[0][1])
-
-# LOCATION: Centroid on x-axis
-def locCntX(t1, t2):
-  return abs(geometry.centroid(t1)[0] - geometry.centroid(t2)[0])
-
-# LOCATION: Centroid on y-axis
-def locCntY(t1, t2):
-  return abs(geometry.centroid(t1)[1] - geometry.centroid(t2)[1])
-
-# ROTATION: Based on orienting spot
-def rotOri(t1, t2):
+def orientation_distance(t1, t2):
   t1_rot = geometry.rotation(t1)
   t2_rot = geometry.rotation(t2)
-  return smallest_rot(t1_rot, t2_rot)
-
-# ROTATION: Based on thinnest vertex
-def rotThin(t1, t2):
-  t1_rot = geometry.rotation_by_smallest_angle(t1)
-  t2_rot = geometry.rotation_by_smallest_angle(t2)
-  return smallest_rot(t1_rot, t2_rot)
-
-# SHAPE: Angle of thinnest vertex
-def shpThin(t1, t2):
-  return abs(geometry.smallest_angle(t1) - geometry.smallest_angle(t2))
-
-# SHAPE: Angle of widest vertex
-def shpWide(t1, t2):
-  return abs(geometry.largest_angle(t1) - geometry.largest_angle(t2))
-
-# SHAPE: Equilateralness ratio
-def shpEqui(t1, t2):
-  return abs(geometry.equilateralness(t1) - geometry.equilateralness(t2))
-
-# BOUNDING BOX RELATIONSHIP: Distance from orienting spot to nearest corner
-def boxOriC(t1, t2):
-  return abs(geometry.dist_to_nearest_corner(t1,0) - geometry.dist_to_nearest_corner(t2,0))
-
-# BOUNDING BOX RELATIONSHIP: Distance from orienting spot to nearest edge
-def boxOriE(t1, t2):
-  return abs(geometry.dist_to_nearest_edge(t1,0) - geometry.dist_to_nearest_edge(t2,0))
-
-# BOUNDING BOX RELATIONSHIP: Mean distance from vertices to nearest corner
-def boxVrtC(t1, t2):
-  dists1 = [geometry.dist_to_nearest_corner(t1,i) for i in range(0,3)]
-  dists2 = [geometry.dist_to_nearest_corner(t2,i) for i in range(0,3)]
-  return abs(np.mean(dists1) - np.mean(dists2))
-
-# BOUNDING BOX RELATIONSHIP: Mean distance from vertices to nearest edge
-def boxVrtE(t1, t2):
-  dists1 = [geometry.dist_to_nearest_edge(t1,i) for i in range(0,3)]
-  dists2 = [geometry.dist_to_nearest_edge(t2,i) for i in range(0,3)]
-  return abs(np.mean(dists1) - np.mean(dists2))
-
-########################################
-
-# Given two rotations from North, return the shortest rotational distance.
-# Required for rotOri() and rotThin()
-
-def smallest_rot(t1_rot, t2_rot):
   # If both rotations have the same sign...
   if t1_rot * t2_rot > 0:
     # ... just return the absolute difference
@@ -152,17 +70,28 @@ def smallest_rot(t1_rot, t2_rot):
       return (2 * np.pi) - theta
     return theta
 
+# Absolute distance between cetroid sizes
+
+def size_distance(t1, t2):
+  return abs(geometry.centroid_size(t1) - geometry.centroid_size(t2))
+
+# Absolute difference between equilateralness ratios
+
+def shape_distance(t1, t2):
+  return abs(geometry.equilateralness(t1) - geometry.equilateralness(t2))
+
 ########################################
 
 # Given a sparse distance matrix, return the most similar and most dissimilar pairs
 
-def most_and_least_similar_pairs(matrix):
+def most_and_least_similar_pairs(distance_array):
+  distance_matrix = squareform(distance_array, force='tomatrix')
   similar_score = 1
   dissimilar_score = 0
-  n = matrix.shape[0]
+  n = distance_matrix.shape[0]
   for i in range(0, n):
     for j in range(i+1, n):
-      score = matrix[i, j]
+      score = distance_matrix[i, j]
       if score < similar_score:
         similar_score = score
         similar_indices = (i, j)
@@ -235,7 +164,7 @@ def generation_results(experiment, chain, generation):
     # Iterate through feature combinations to find the best correlation
     best_r = -1
     for i in range(0, len(all_combination_matrices)):
-      r = np.corrcoef(string_distances, squareform(all_combination_matrices[i]))[0,1]
+      r = np.corrcoef(string_distances, all_combination_matrices[i])[0,1]
       if r > best_r:
         best_matrix = i
         best_r = r
@@ -245,6 +174,7 @@ def generation_results(experiment, chain, generation):
 
 ########################################
 
-metrics_by_feature = [[boxOriC, boxOriE, boxVrtC, boxVrtE], [locOriX, locOriY, locCntX, locCntY], [rotOri, rotThin], [shpThin, shpWide, shpEqui], [sizArea, sizPeri, sizCent]]
+metrics = [location_distance, orientation_distance, shape_distance, size_distance]
 static_set_triangles = basics.getTriangles(1, 'A', 0, 's')
-all_combination_matrices = combination_matrices(static_set_triangles, metrics_by_feature)
+individual_matrices = feature_matrices(static_set_triangles, metrics)
+all_combination_matrices = combination_matrices(individual_matrices)
