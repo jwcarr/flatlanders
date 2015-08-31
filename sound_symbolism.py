369f6726287a0af9505d1a58ebdc8167d39320a8
Jon Carr <jwcarr@users.noreply.github.com>
2015-08-31 17:11:53 +0100

    Made monte carlo non-optional and changed argument name to permutations

--- sound_symbolism.py
+++ sound_symbolism.py
@@ -16,25 +16,25 @@ bigness_phonemes = [['b', 'd', 'g', 'l', 'm', 'w', 'AA', 'OW', 'AO', 'UW'], ['k'
 
 # Functions for generating experiment, chain, or generation results
 
-def experiment_results(experiment, set_type='s', symbolism='shape', monte_carlo=False):
+def experiment_results(experiment, set_type='s', symbolism='shape', permutations=1000):
   experiment_results = []
   for chain in basics.chain_codes[experiment-1]:
-    experiment_results.append(chain_results(experiment, chain, set_type, symbolism, monte_carlo))
+    experiment_results.append(chain_results(experiment, chain, set_type, symbolism, permutations))
   return experiment_results
 
-def chain_results(experiment, chain, set_type='s', symbolism='shape', monte_carlo=False):
+def chain_results(experiment, chain, set_type='s', symbolism='shape', permutations=1000):
   chain_results = []
   for generation in range(0, 11):
-    chain_results.append(generation_results(experiment, chain, generation, set_type, symbolism, monte_carlo))
+    chain_results.append(generation_results(experiment, chain, generation, set_type, symbolism, permutations))
   return chain_results
 
-def generation_results(experiment, chain, generation, set_type, symbolism='shape', monte_carlo=False):
+def generation_results(experiment, chain, generation, set_type, symbolism='shape', permutations=1000):
   words = basics.getWords(experiment, chain, generation, set_type)
   triangles = basics.getTriangles(experiment, chain, generation, set_type)
   if symbolism == 'shape':
-    return correlate_form_and_symbolism(words, roundedness_phonemes, triangles, geometry.equilateralness, monte_carlo)
+    return correlate_form_and_symbolism(words, roundedness_phonemes, triangles, geometry.equilateralness, permutations)
   elif symbolism == 'size':
-    return correlate_form_and_symbolism(words, bigness_phonemes, triangles, geometry.centroid_size, monte_carlo)
+    return correlate_form_and_symbolism(words, bigness_phonemes, triangles, geometry.centroid_size, permutations)
   else:
     raise ValueError('Invalid symbolism argument. Should be "shape" or "size".')
 
@@ -44,17 +44,15 @@ def generation_results(experiment, chain, generation, set_type, symbolism='shape
 # metric, correlate the scores. If monte_carlo is False, just return the Pearson
 # correlation coefficient, else use Monte_Carlo() and return a z-score.
 
-def correlate_form_and_symbolism(words, symbolic_phonemes, triangles, triangle_metric, monte_carlo=False):
+def correlate_form_and_symbolism(words, symbolic_phonemes, triangles, triangle_metric, permutations=1000):
   word_scores = np.asarray([score_word(word, symbolic_phonemes) for word in words], dtype=int)
   triangle_scores = np.asarray([triangle_metric(triangle) for triangle in triangles], dtype=float)
-  if type(monte_carlo) == int:
-    return Monte_Carlo(word_scores, triangle_scores, monte_carlo)
-  return np.corrcoef(word_scores, triangle_scores)[0,1]
+  return Monte_Carlo(word_scores, triangle_scores, permutations)
 
 # Given word scores and triangle scores, randomize the mapping between them a large
 # number of times and compute a z-score for the significance of the veridical correlation
 
-def Monte_Carlo(word_scores, triangle_scores, permutations):
+def Monte_Carlo(word_scores, triangle_scores, permutations=1000):
   correlations = np.zeros(permutations, dtype=float)
   correlations[0] = np.corrcoef(word_scores, triangle_scores)[0,1]
   for i in range(1, permutations):
