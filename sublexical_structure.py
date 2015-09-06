280b19f2e8545d7b3603b95f4c9e240f24b9d2f2
Jon Carr <jwcarr@users.noreply.github.com>
2015-09-06 13:03:32 +0100

    Optimized sublexical_structure.py - now 20 times faster

--- sublexical_structure.py
+++ sublexical_structure.py
@@ -1,82 +1,56 @@
 from itertools import permutations
 from math import factorial
+from scipy.spatial.distance import squareform
 import numpy as np
 import basics
-import rater_analysis
 
-# Run a categorical Mantel test. This is like a regular Mantel test, except that you're
-# shuffling the categories that meanings belong to rather than shuffling the string-meaning
-# mapping freely.
+def test(strings, meaning_distances, perms):
 
-def test(experiment, chain, generation, set_type, randomizations):
-  words = basics.getWords(experiment, chain, generation, set_type)
-  word_distances = basics.stringDistances(words)
-  categorized_words, categorized_meanings = CategorizeWords(words)
-  meaning_distances = rater_analysis.reliable_distance_array
-  r = np.corrcoef(meaning_distances, word_distances)[0,1]
-  n = len(words)
-  m, sd = Mantel(meaning_distances, categorized_words, categorized_meanings, r, n, generation, randomizations)
-  z = (r-m)/sd
-  return r, m, sd, z
+  # Determine category labels and the edit-distances between them
+  category_labels = list(set(strings))
+  label_distances = squareform(basics.stringDistances(category_labels), force='tomatrix')
 
-# Takes a list of strings and returns two lists: a list of the unique strings, and another
-# list of the same length which gives the meaning indices that the words map on to. Not using
-# a dictionary because this can potentially create issues with the shuffling because
-# dictionaries don't have order.
+  # Compute meaning distance residuals
+  meaning_residuals = residualize(meaning_distances)
 
-def CategorizeWords(words):
-  categorized_words = []
-  categorized_meanings = []
-  for i in xrange(len(words)):
-    try:
-      position = categorized_words.index(words[i])
-      categorized_meanings[position].append(i)
-    except ValueError:
-      categorized_words.append(words[i])
-      categorized_meanings.append([i])
-  return categorized_words, categorized_meanings
+  # Number of strings, number of categories, and number of category permutations
+  m = len(strings)
+  n = len(category_labels)
+  p = factorial(n)
 
-# If the number of possible mappings is less than the randomizations parameter, conduct a
-# deterministic Mantel test; otherwise, do a stochasitc Mantel test.
+  # Deterministic test - measure every possible category-meaning mapping
+  if p <= perms:
+    covariences = np.zeros(p, dtype=float)
+    for i, order in enumerate(permutations(range(n))):
+      permuted_categories = [category_labels[j] for j in order]
+      string_distances = []
+      for j in range(0, m):
+        idx1 = permuted_categories.index(strings[j])
+        for k in range(j+1, m):
+          idx2 = permuted_categories.index(strings[k])
+          string_distances.append(label_distances[idx1, idx2])
+      string_residuals = residualize(string_distances)
+      covariences[i] = (meaning_residuals * string_residuals).sum()
 
-def Mantel(meaning_distances, categorized_words, categorized_meanings, r, n, generation, randomizations):
-  if factorial(len(categorized_words)) <= randomizations:
-    print('    Generation %d: Only %d possible permutations - computing result deterministically' % (generation, factorial(len(categorized_words))))
-    return DeterministicMantel(meaning_distances, categorized_words, categorized_meanings, n)
-  return StochasticMantel(meaning_distances, categorized_words, categorized_meanings, r, n, randomizations)
+  # Stochasitc test - randomly sample the space of category-meaning mappings
+  else:
+    string_residuals = residualize(basics.stringDistances(strings))
+    covariences = np.zeros(perms, dtype=float)
+    covariences[0] = (meaning_residuals * string_residuals).sum()
+    for i in range(1, perms):
+      np.random.shuffle(category_labels)
+      string_distances = []
+      for j in range(0, m):
+        idx1 = category_labels.index(strings[j])
+        for k in range(j+1, m):
+          idx2 = category_labels.index(strings[k])
+          string_distances.append(label_distances[idx1, idx2])
+      string_residuals = residualize(string_distances)
+      covariences[i] = (meaning_residuals * string_residuals).sum()
 
-# Stochasitc Mantel test - randomly sample the space of category-meaning mappings
+  # Return standard score (z-score)
+  return (covariences[0] - covariences.mean()) / covariences.std()
 
-def StochasticMantel(meaning_distances, categorized_words, categorized_meanings, r, n, randomizations):
-  correlations = np.zeros(randomizations, dtype=float)
-  correlations[0] = r
-  for i in xrange(1, randomizations):
-    correlations[i] = np.corrcoef(meaning_distances, GeneratePermutation(categorized_words, categorized_meanings, n))[0,1]
-  return correlations.mean(), correlations.std()
-
-def GeneratePermutation(categorized_words, categorized_meanings, n):
-  np.random.shuffle(categorized_words)
-  permuted_words = [None] * n
-  for i in range(len(categorized_meanings)):
-    for j in categorized_meanings[i]:
-      permuted_words[j] = categorized_words[i]
-  return basics.stringDistances(permuted_words)
-
-# Deterministic Mantel test - measure every possible category-meaning mapping
-
-def DeterministicMantel(meaning_distances, categorized_words, categorized_meanings, n):
-  m = len(categorized_words)
-  correlations = np.zeros(factorial(m), dtype=float)
-  orders = permutations(range(m))
-  i = 0
-  for order in orders:
-    correlations[i] = np.corrcoef(meaning_distances, GenerateDeterministicPermutation(categorized_words, categorized_meanings, order, n))[0,1]
-    i += 1
-  return correlations.mean(), correlations.std()
-
-def GenerateDeterministicPermutation(categorized_words, categorized_meanings, order, n):
-  permuted_words = [None] * n
-  for i in range(len(categorized_meanings)):
-    for j in categorized_meanings[i]:
-      permuted_words[j] = categorized_words[order[i]]
-  return basics.stringDistances(permuted_words)
+def residualize(distances):
+  distances = np.array(distances, dtype=float)
+  return distances - distances.mean()
